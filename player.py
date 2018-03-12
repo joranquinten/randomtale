d23a9ca6d228601e4152c8b2f7822adaff9e9a44
Joran Quinten <joranquinten@hotmail.com>
2018-03-12 14:42:13 +0100

    removed keyboard input

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 def player_load(music_file, volume=0.8):
-    print("Loading")
+    print("player_load")
     '''
     stream music with mixer.music module in a blocking manner
     this will stream the sound from disk while playing
@@ -14,34 +14,24 @@ def player_load(music_file, volume=0.8):
     pg.mixer.init(freq, bitsize, channels, buffer)
     # volume value 0.0 to 1.0
     pg.mixer.music.set_volume(volume)
-    clock = pg.time.Clock()
     try:
         pg.mixer.music.load(music_file)
         print("Music file {} loaded!".format(music_file))
     except pg.error:
         print("File {} not found! ({})".format(music_file, pg.get_error()))
         return
-    pg.mixer.music.play()
-    while pg.mixer.music.get_busy():
-        # check if playback has finished
-        clock.tick(30)
 
 def player_start():
-    print("Started")
+    print("player_start")
+    clock = pg.time.Clock()
     if pg.mixer.music.get_busy() == False:
         pg.mixer.music.play()
+        while pg.mixer.music.get_busy():
+            # check if playback has finished
+            clock.tick(30)
+
 
 def player_stop():
-    print("Stopped")
+    print("player_stop")
     if pg.mixer.music.get_busy():
         pg.mixer.music.stop()
-
-def player_pause():
-    print("Paused")
-    if pg.mixer.music.get_busy():
-        pg.mixer.music.pause()
-
-def player_resume():
-    print("Resumed")
-    if pg.mixer.music.get_busy():
-        pg.mixer.music.unpause()
